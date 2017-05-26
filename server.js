e43fc62951559bc5f56ae160429b5b269967ad7d
Kim YoonJin <indeu@hotmail.com>
2017-05-26 16:13:04 +0900

    server.js 수정

--- server.js
+++ server.js
@@ -68,12 +68,14 @@ app.get('/', function (req, res) {
     var col = db.collection('counts');
     // Create a document with request IP and current time of request
     col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
+    // col.count(function(err, count){
+    //   res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    // });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    //res.render('index.html', { pageCountMessage : null});
   }
+
+  res.render('index.html');
 });
 
 app.get('/pagecount', function (req, res) {
